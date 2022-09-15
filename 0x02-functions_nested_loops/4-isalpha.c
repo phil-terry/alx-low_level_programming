dbb2407379402493507ad4cb53eed55ce635ccac
phil-terry <philterry26@gmail.com>
2022-09-14 21:00:04 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /**
- *
  * _isalpha - function that checks for alphabetic character.
- *
  * @c: The input character to check
- *
  * Return: int.
- *
  */
 
 int _isalpha(int c)
