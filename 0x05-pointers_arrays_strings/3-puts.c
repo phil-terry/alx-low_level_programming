5e11ad274d6d74bedb8ed5e4b36c29bb349b3a6b
phil-terry <philterry26@gmail.com>
2022-09-19 20:51:16 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,14 +10,14 @@ void _puts(char *str)
 {
 	int count = 0;
 
-	while (count >= 0)
-	{
-		if (str[count] == '\0')
-		{
+	while (count >= 0
+			{
+			if (str[count] == '\0')
+			{
 				_putchar('\n');
 				break;
-		}
-		_putchar(str[count]);
-		count++;
+			}
+			_putchar(str[count]);
+			count++;
 	}
 }
